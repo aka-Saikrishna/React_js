aa9c176b4cb33c1132d7bf6b2d4eb8ef071036c1
aka-Saikrishna <vallalasaikrishna04@gmail.com>
2024-05-16 19:26:33 +0530

    03 - jsx info

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ import ReactDOM from "react-dom"
 // JSX - transpiled before it reaches to JS by parcelwith the help of babel
 // How JSX works 
 // JSX => React.createElement => ReactElemet - JS object => HTMLElement(render)
-const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>
+const heading = 
+ (<h1 
+className="heading">
+    Namaste React using JSX
+</h1>)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render( jsxHeading);
+root.render( heading);
